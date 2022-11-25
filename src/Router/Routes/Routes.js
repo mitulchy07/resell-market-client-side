331b4825a1bb4a3f1479b57e6dce68e66be4094e
mitulchy07 <mitul9616@gmail.com>
2022-11-25 16:02:20 +0100

    router setup done

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,20 +1,15 @@
-import React, { useContext } from 'react';
-import { Spinner } from 'react-bootstrap';
-import { Navigate, useLocation } from 'react-router-dom';
-import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../../layout/Main';
 
-const PrivateRoute = ({ children }) => {
-  const { user, loading } = useContext(AuthContext);
-  const location = useLocation();
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+  },
+  {
+    path: '*',
+    element: 'No Route found here.',
+  },
+]);
 
-  if (loading) {
-    return <Spinner animation='border' />;
-  }
-
-  if (user && user.uid) {
-    return children;
-  }
-  return <Navigate to='/login' state={{ from: location }} replace></Navigate>;
-};
-
-export default PrivateRoute;
+export default router;
---
+++ src/layout/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      <h1>This is Main</h1>
+    </div>
+  );
+};
+
+export default Main;
