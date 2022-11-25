98c12a4cd824bda45d65ec5e3cd2def179ffa17f
mitulchy07 <mitul9616@gmail.com>
2022-11-25 21:26:51 +0100

    login and registration page done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [data, setData] = useState('');
   return (
-    <div>
-      <Link to='/register' className='btn m-4'>
-        Register Here.
-      </Link>
-    </div>
+    <form
+      className='grid grid-cols-1 gap-2 max-w-lg m-4'
+      onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}
+    >
+      <input
+        className='input btn-success btn-outline'
+        {...register('email', { required: 'Email address is requierd' })}
+        placeholder='Email'
+        type='email'
+      />
+      {errors.email && (
+        <p className='text-red-600'> {errors.email?.message} </p>
+      )}
+      <input
+        className='input btn-success btn-outline'
+        {...register('password', { required: 'Password is required' })}
+        placeholder='Password'
+        type='password'
+      />
+      {errors.password && (
+        <p className='text-red-600'> {errors.password?.message} </p>
+      )}
+      <p>{data}</p>
+
+      <Link to='/register'>Need an account?</Link>
+      <input className='input btn-success btn-outline' type='submit' />
+    </form>
   );
 };
 
--- src/Pages/Login/Register.js
+++ src/Pages/Login/Register.js
@@ -1,10 +1,80 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { createUser } = useContext(AuthContext);
+  const handleSignUp = (data) => {
+    createUser(data.email, data.password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((err) => console.error(err));
+  };
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <form
+      className='grid grid-cols-1 gap-2 max-w-lg m-4'
+      onSubmit={handleSubmit(handleSignUp)}
+    >
+      <input
+        className='input btn-success btn-outline'
+        type='name'
+        {...register('name', { required: 'Name is requierd' })}
+        placeholder='Full Name'
+      />
+      {errors.name && <p className='text-red-600'> {errors.name?.message} </p>}
+      <input
+        className='input btn-success btn-outline'
+        {...register('email', { required: 'Email address is requierd' })}
+        type='email'
+        placeholder='Email'
+      />
+      {errors.email && (
+        <p className='text-red-600'> {errors.email?.message} </p>
+      )}
+
+      <input
+        className='input btn-success btn-outline'
+        {...register('password', {
+          required: 'Password is requierd',
+          minLength: {
+            value: 6,
+            message: 'Password should be minimum 6 characters.',
+          },
+        })}
+        type='password'
+        placeholder='Password'
+      />
+      {errors.password && (
+        <p className='text-red-600'> {errors.password?.message} </p>
+      )}
+      <div className='text-white m-5'>
+        <input
+          {...register('status', { required: true })}
+          type='radio'
+          value='Buyer'
+          className='radio radio-secondary'
+          checked
+        />
+        <span className='mx-5'>Buyer</span>
+        <input
+          {...register('status', { required: true })}
+          type='radio'
+          value='Seller'
+          className='radio radio-secondary'
+        />
+        <span className='mx-5'>Seller</span>
+      </div>
+      <Link to='/login'>Already have an account?</Link>
+      <input className='input btn-success btn-outline' type='submit' />
+    </form>
   );
 };
 
